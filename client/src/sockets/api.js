174b89f9ee04283a78b4e0181026de3068928b39
Harry <h.woodall311@hotmail.co.uk>
2019-01-23 13:46:31 +0000

    client side socket fix again

--- client/src/sockets/api.js
+++ client/src/sockets/api.js
@@ -1,5 +1,5 @@
 import openSocket from "socket.io-client";
-const socket = openSocket();
+const socket = openSocket("http://localhost:8000");
 
 function socketAPI() {
   socket.on("start", function(data) {
