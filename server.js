7c469857939dd37fc40ad043a7f422de7dce8e97
Harry <h.woodall311@hotmail.co.uk>
2019-01-24 11:17:33 +0000

    refactoring sockets

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 const server = app.listen(port, () =>
   console.log("Server started on port " + port)
 );
-const io = socket(server);
+const io = new sockets(server);
 
 app.get("/api/test", (req, res) => {
   const test = [{ id: 0, item: 1 }, { id: 1, item: 2 }, { id: 3, item: 3 }];
@@ -28,8 +28,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/public/index.html"));
 });
 
-//io.startSocket();
-io.on("connection", socket => {
-  console.log("user connected");
-  socket.emit("start", { Hello: "World" });
-});
+io.startSocket();
